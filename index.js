05e184ece8f8c1783610d71843d1f6db9b80b25d
drekka <d4rkf1br3@gmail.com>
2024-11-12 15:15:49 +1100

    function?

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 const core = require('@actions/core')
 const glob = require('@actions/glob')
 
-try {
+function generateReport() {
+    try {
 
-    core.summary.addHeading('Code coverage', '1')
-    core.summary.addRaw('Code coverage results', true)
+        core.summary.addHeading('Code coverage', '1')
+        core.summary.addRaw('Code coverage results', true)
 
-    const globber = await glob.create('**/*.json', {followSymbolicLinks: false})
-    for await (const file of globber.glob()) {
-        console.log('JSON file found: ' + file)
+        const globber = await glob.create('**/*.json', {followSymbolicLinks: false})
+        for await (const file of globber.glob()) {
+            console.log('JSON file found: ' + file)
+        }
+
+    } catch (error) {
+        // Handle errors and indicate failure
+        core.setFailed(error.message);
     }
-
-} catch (error) {
-    // Handle errors and indicate failure
-    core.setFailed(error.message);
 }
+
+await generateReport()
