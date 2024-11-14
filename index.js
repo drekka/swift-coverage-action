fdbdc8c790661d9ff09161ed9d5e66320ea2639a
drekka <d4rkf1br3@gmail.com>
2024-11-14 17:45:48 +1100

    Coverting to a class

--- index.js
+++ index.js
@@ -6,121 +6,126 @@ const path = require('node:path')
 
 const micromatch = require('micromatch')
 
-async function generateReport() {
-    try {
+class CoverageChecker {
 
-        // We need the workspace directory so we can slice it off the file paths.
-        const projectDir = process.env["GITHUB_WORKSPACE"]
-        console.log('Project directory: ' + projectDir)
+    // We need the workspace directory so we can slice it off the file paths.
+    #projectDir = process.env["GITHUB_WORKSPACE"]
+    #buildDir = core.getInput('build-dir', { required: true })
+    #minCoverage = core.getInput('coverage', { required: true })
+    #showAllFiles = core.getBooleanInput('show-all-files', { required: true })
+    #coverageFileSource
+    #includes
+    #excludes
 
-        const buildDir = core.getInput('build-dir')
-        const coverageFileFilter = core.getInput('coverage-files')
-        const coverageFilter = path.join(buildDir, coverageFileFilter)
-        const minCoverage = core.getInput('coverage')
+    constructor() {
+        console.log('Project directory: ' + #projectDir)
+        const coverageFileFilter = core.getInput('coverage-files', { required: true })
+        this.#coverageFileSource = path.join(buildDir, coverageFileFilter)
+        this.#includes = this.#readFilterGlobs('Reporting on files matching', 'includes')
+        this.#excludes = this.#readFilterGlobs('Excluding files matching', 'excludes')
+    }
 
-        console.log('Loading coverage from: ' + coverageFilter)
+    async function generateReport() {
+        try {
+            console.log('Loading coverage from: ' + #coverageFileSource)
+            const globber = await glob.create(this.#coverageFileSource, {followSymbolicLinks : false})
+            const coverageFiles = await globber.glob()
+            for (const coverageFile of coverageFiles) {
+                this.#processCoverage(coverageFile)
+            }
 
-        const includes =
-        readFilterGlobs('Reporting on files matching', 'includes', buildDir)
-        const excludes =
-        readFilterGlobs('Excluding files matching', 'excludes', buildDir)
+        } catch (error) {
+            // Handle errors and indicate failure
+            core.setFailed(error.message);
+        }
+    }
 
-        const globber =
-        await glob.create(coverageFilter, {followSymbolicLinks : false})
-        const coverageFiles = await globber.glob()
-        for (const coverageFile of coverageFiles) {
-            processCoverage(coverageFile, includes, excludes, buildDir, minCoverage, projectDir)
+    // Reads a filter from the input arguments and generates a list of globs.
+    function #readFilterGlobs(logTitle, input) {
+        return core.getInput(input).split(',')
+        .filter(glob => glob.trim())
+        .map(glob => {
+            console.log(logTitle + ': ' + glob)
+            return glob
+        })
+    }
+
+    // Processes a single coverage file.
+    #function #processCoverage(file) {
+
+        console.log('Reading coverage file: ' + file)
+
+        fs.readFile(file, (err, rawData) => {
+
+            if (err) throw err;
+
+            // Parse the raw coverage JSON into a data structure.
+            const coverage = JSON.parse(rawData);
+
+            // Read the file coverage summaries, rejecting files from the build dir as they'll be dependencies.
+            var coverageData = coverage.data[0].files.filter(fileCoverage => fileCoverage.filename.indexOf(this.#buildDir) == -1)
+
+            // Filter the coverage data using the specified Globs.
+            coverageData = filter(coverageData, this.#includes)
+            coverageData = filter(coverageData, this.#excludes, true)
+
+            // Build the report.
+            console.log('Coverage on ' + coverageData.length + ' files being processed…')
+            var failedCoverage = []
+            coverageData.forEach(coverage => {
+                const lines = coverage.summary.lines
+                console.log('File: ' + coverage.filename + ', lines: ' + lines.count + ', coverage: ' + lines.percent.toFixed(2) + '%')
+                if (lines.percent < this.#minCoverage) {
+                    failedCoverage.push(coverage)
+                }
+            })
+
+            // Generate the coverage report.
+            report(failedCoverage, failedCoverage.length == 0)
+        });
+    }
+
+    // Generates the coverage report.
+    function #report(coverageData, success) {
+
+        const summary = core.summary
+        summary.addHeading('Coverage report', '1')
+
+        if (success) {
+            summary.addRaw('Coverage is above ' + this.#minCoverage + '%.', true).write()
+            return
         }
 
-    } catch (error) {
-        // Handle errors and indicate failure
-        core.setFailed(error.message);
-    }
-}
+        // Failed coverage.
+        const tableData = [[
+            {data : 'File', header : true},
+            {data : 'LOC', header : true},
+            {data : 'Coverage', header : true}
+        ]]
 
-// Reads a filter from the input arguments and generates a list of globs.
-function readFilterGlobs(logTitle, input, buildDir) {
-    return core.getInput(input).split(',')
-    .filter(glob => glob.trim())
-    .map(glob => {
-        console.log(logTitle + ': ' + glob)
-        return glob
-    })
-}
-
-// Processes a single coverage file.
-function processCoverage(file, includes, excludes, buildDir, minCoverage, projectDir) {
-
-    console.log('Reading coverage file: ' + file)
-
-    fs.readFile(file, (err, rawData) => {
-
-        if (err) throw err;
-
-        // Parse the raw coverage JSON into a data structure.
-        const coverage = JSON.parse(rawData);
-
-        // Read the file coverage summaries, rejecting files from the build dir as they'll be dependencies.
-        var coverageData = coverage.data[0].files.filter(fileCoverage => fileCoverage.filename.indexOf(buildDir) == -1)
-
-        // Filter the coverage data using the specified Globs.
-        coverageData = filter(coverageData, includes)
-        coverageData = filter(coverageData, excludes, true)
-
-        // Build the report.
-        console.log('Coverage on ' + coverageData.length + ' files being processed…')
-        var failedCoverage = []
+        let projectDirIndex = this.#projectDir.length
         coverageData.forEach(coverage => {
             const lines = coverage.summary.lines
-            console.log('File: ' + coverage.filename + ', lines: ' + lines.count + ', coverage: ' + lines.percent.toFixed(2) + '%')
-            if (lines.percent < minCoverage) {
-                failedCoverage.push(coverage)
-            }
+            tableData.push([{data : coverage.filename.slice(projectDirIndex) }, {data : lines.count}, {data: lines.percent.toFixed(2) + '%'}])
         })
+        summary.addTable(tableData)
+        summary.write()
 
-        // Generate the coverage report.
-        report(failedCoverage, failedCoverage.length == 0, minCoverage, projectDir)
-    });
-}
-
-// Generates the coverage report.
-function report(coverageData, success, minCoverage, projectDir) {
-
-    const summary = core.summary
-    summary.addHeading('Coverage report', '1')
-
-    if (success) {
-        summary.addRaw('Coverage is above ' + minCoverage + '%.', true).write()
-        return
+        core.setFailed(`Coverage below ` + this.#minCoverage + '%');
     }
 
-    // Failed coverage.
-    const tableData = [[
-        {data : 'File', header : true},
-        {data : 'LOC', header : true},
-        {data : 'Coverage', header : true}
-    ]]
+    // Returns a list of glob filtered coverage data.
+    function #filter(coverageData, globs, invert = false) {
 
-    let projectDirIndex = projectDir.length
-    coverageData.forEach(coverage => {
-        const lines = coverage.summary.lines
-        tableData.push([{data : coverage.filename.slice(projectDirIndex) }, {data : lines.count}, {data: lines.percent.toFixed(2) + '%'}])
-    })
-    summary.addTable(tableData)
-    summary.write()
+        if (globs.length == 0) {
+            return coverageData
+        }
 
-    core.setFailed(`Coverage below ` + minCoverage + '%');
-}
-
-// Returns a list of glob filtered coverage data.
-function filter(coverageData, globs, invert = false) {
-
-    if (globs.length == 0) {
-        return coverageData
+        const matcher = micromatch.matcher(globs)
+        return coverageData.filter(coverage => matcher(coverage.filename) && !invert)
     }
 
-    const matcher = micromatch.matcher(globs)
-    return coverageData.filter(coverage => matcher(coverage.filename) && !invert)
 }
 
-generateReport()
+
+CoverageChecker().generateReport()
