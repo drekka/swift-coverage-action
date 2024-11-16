7cf14deb4237144232507a573f33370c28f911c4
Derek Clarkson <d4rkf1br3@gmail.com>
2024-11-16 13:04:32 +1100

    typo

--- index.js
+++ index.js
@@ -119,7 +119,7 @@ class CoverageChecker {
 
         let projectDirIndex = this.#projectDir.length
         coverageData
-        .toSorted(this.#sortByName ? this.#sortCoverageByName : this.@sortCoveragebyPct)
+        .toSorted(this.#sortByName ? this.#sortCoverageByName : this.#sortCoveragebyPct)
         .forEach(coverage => {
             const lines = coverage.summary.lines
             const failedCoverage = lines.percent < this.#minCoverage
